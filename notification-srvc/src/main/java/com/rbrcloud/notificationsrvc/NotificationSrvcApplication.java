ebcdba5b9eec0d3daa2735d75f2c94f4a627572a
Rajesh <rajesh.spk@gmail.com>
2026-02-08 06:37:22 -0800

    Add kafka listener config

--- notification-srvc/src/main/java/com/rbrcloud/notificationsrvc/NotificationSrvcApplication.java
+++ notification-srvc/src/main/java/com/rbrcloud/notificationsrvc/NotificationSrvcApplication.java
@@ -2,10 +2,13 @@ package com.rbrcloud.notificationsrvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class NotificationSrvcApplication {
     public static void main(String[] args) {
+        System.out.println(">>> CHECKING IF REBUILD WORKED <<<");
         SpringApplication.run(NotificationSrvcApplication.class, args);
     }
 }
